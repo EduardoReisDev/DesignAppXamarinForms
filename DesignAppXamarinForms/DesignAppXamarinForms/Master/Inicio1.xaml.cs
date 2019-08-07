d3798ad2b613476a583ef5f5bda845a56491eee0
EduardoReisDev <52722526+EduardoReisDev@users.noreply.github.com>
2019-08-07 19:28:14 -0300

    07/08 (01)

--- DesignAppXamarinForms/DesignAppXamarinForms/Master/Inicio1.xaml.cs
+++ DesignAppXamarinForms/DesignAppXamarinForms/Master/Inicio1.xaml.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DesignAppXamarinForms.Master
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Inicio1 : ContentPage
+    public partial class Inicio1 : MasterDetailPage
     {
         public Inicio1()
         {
             InitializeComponent();
+        }
+
+        private void GoPaginaPerfil1(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
+        }
+
+        private void GoPaginaXamarin(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Pages.Xamarin());
+            IsPresented = false;
         }
     }
 }
